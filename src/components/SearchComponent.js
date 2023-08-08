bfe22a28d6fcdc4513c24a22e0110c9009304397
ianotieno <ianotieno23@gmail.com>
2023-08-08 18:44:03 +0300

    update

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -12,7 +12,7 @@ const SearchComponent = ({ searchQuery }) => {
   );
 
   return (
-    <div>
+    <div className='place-content:center'>
      <div className=" w-half h-half bg-white/10 fixed z-10 top-10 py-12 grid md:grid-cols-4 gap-6 text-gray-500">
       {filteredName.length === 0 ? (
         <p className='font-bold text-2xl px-2 pt-4'><span>No matching products found</span></p> 
@@ -20,7 +20,7 @@ const SearchComponent = ({ searchQuery }) => {
         filteredName.map((item,index) => (
           <div className='relative' key={index}>
                <p className='font-bold text-2xl px-2 pt-4'><span> { `${item.name}`}</span></p>
-              <img className='max-h-[100px] md:max-h-[200px]  object-cover rounded-xl'src={item.image} alt={item.name} style={{ height: 70, width: 60 }} />
+              <img className='max-h-[250px] md:max-h-[250px]  object-cover rounded-xl'src={item.image} alt={item.name} style={{ height: 70, width: 60 }} />
               <p className='font-bold text-orange-600 text-2xl px-2 pt-4'><span> { `${item.price}`}</span></p>
                
           </div>
