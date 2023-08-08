0ae2a788147002c5f53ac2f3b2877f3d9020a5f2
ianotieno <ianotieno23@gmail.com>
2023-08-09 00:28:07 +0300

    order button

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -13,16 +13,16 @@ const SearchComponent = ({ searchQuery }) => {
 
   return (
     <div >
-     <div className=" w-half h-half bg-white/10 fixed z-10 top-10 right-20 py-12 grid md:grid-cols-4 gap-6 text-gray-500">
+     <div className=" w-half h-half bg-white/10 fixed z-10 top-10 right-20 py-12 grid md:grid-cols-4 gap-6 text-white">
       {filteredName.length === 0 ? (
-        <p className='font-bold text-2xl px-2 pt-4'><span>No matching products found</span></p> 
+        <p className='relative font-bold text-2xl px-2 pt-4'><span>No matching products found</span></p> 
       ) : (
         filteredName.map((item,index) => (
           <div className='relative' key={index}>
                <p className='font-bold text-2xl px-2 pt-4'><span> { `${item.name}`}</span></p>
-              <img className='max-h-[250px] md:max-h-[250px]  object-cover rounded-xl'src={item.image} alt={item.name} style={{ height: 70, width: 60 }} />
+              <img className='w-[60%] h-[350px] object-cover rounded-t-lg  md:max-h-[250px] 'src={item.image} alt={item.name} style={{ height: 70, width: 60 }} />
               <p className='font-bold text-orange-600 text-2xl px-2 pt-4'><span> { `${item.price}`}</span></p>
-               
+              <button className='border-orange-500 bg-white text-black mx-2  bottom-4'>Order Now</button>
           </div>
         ))
       )}
